339d393b78013f624f7a1999c668a72520bd2ca3
Daniil Rose <daniil.rose@member.fsf.org>
2022-02-20 16:53:28 -0500

    Add git blamer

--- config.el
+++ config.el
@@ -197,6 +197,20 @@
 (use-package! hackernews
   :commands (hackernews))
 
+(use-package blamer
+  :bind (("s-i" . blamer-show-commit-info))
+  :defer 20
+  :custom
+  (blamer-idle-time 0.3)
+  (blamer-min-offset 70)
+  :custom-face
+  (blamer-face ((t :foreground "#7a88cf"
+                    :background nil
+                    :height 140
+                    :italic t)))
+  :config
+  (global-blamer-mode 1))
+
 ;;; IRC
 (when catster/use-irc
   (require 'irc-config))
--- packages.el
+++ packages.el
@@ -77,3 +77,5 @@
   :recipe (:host github :repo "ieure/exwm-mff"))
 
 (package! jenkinsfile-mode)
+
+(package! blamer)
